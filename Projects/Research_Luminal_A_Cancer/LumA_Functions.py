64655b8f124be9720631fd8e011d83ec17e5a205
ShlomoAmor <shlomoamor1@gmail.com>
2021-01-24 07:55:14 +0000

    Updated versions

--- Projects/Research_Luminal_A_Cancer/LumA_Functions.py
+++ Projects/Research_Luminal_A_Cancer/LumA_Functions.py
@@ -1,9 +1,11 @@
 import time
 import multiprocessing
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import CategoricalNB
 from functools import partial
 import pandas as pd
 import numpy as np
+from scipy.stats import rankdata
 
 """
 Inputs: 
@@ -15,13 +17,11 @@ Description:
 This function returns the data input in ranked and binned
 """
 def bin_rank(data, bins=5):
-    
     data = pd.DataFrame(data)
     rank_data = data.rank(axis=1)
-    
+    rank_data = rank_data.apply(pd.cut, bins=bins, axis=1, labels=False)
 #     for i, row in rank_data.iterrows():
 #         rank_data.loc[i, :] = pd.cut(row, bins=bins, labels=False)
-    
     return rank_data.values
 
 """
@@ -42,7 +42,8 @@ def find_best_feature(selected_features, X_train, C_param, y_train, feature):
     X_train_filt_ranked = bin_rank(X_train_filt)
     
     # Fit a Logistic regression model
-    lr = LogisticRegression(C=C_param, random_state=0).fit(X_train_filt_ranked, y_train)
+    lr = CategoricalNB(alpha=1.0, fit_prior=True, class_prior=None,min_categories=5).fit(X_train_filt_ranked, y_train)
+    #lr = LogisticRegression(C=C_param, random_state=0).fit(X_train_filt_ranked, y_train)
     # Get the accuracy rate using the validation set
     accu_train = lr.score(X_train_filt_ranked, y_train)
     # Tuple format = (feature number, training accuracy found)
@@ -76,7 +77,8 @@ def selected_feature_check(data, X_train, y_train, selected_features, selected_f
         X_train_filt = X_train[:, temp_features]
         X_train_filt_ranked = bin_rank(X_train_filt)
         # Fit a Logistic regression model
-        lr = LogisticRegression(C=C_param, random_state=0).fit(X_train_filt_ranked, y_train)
+        lr = CategoricalNB(alpha=1.0, fit_prior=True, class_prior=None,min_categories=5).fit(X_train_filt_ranked, y_train)
+        #lr = LogisticRegression(C=C_param, random_state=0).fit(X_train_filt_ranked, y_train)
         # Get the accuracy rate using the validation set
         accu_train = lr.score(X_train_filt_ranked, y_train)
         feature_removal_score[feature] = accu_train
@@ -89,6 +91,9 @@ def selected_feature_check(data, X_train, y_train, selected_features, selected_f
     
     # Check if we have made any changes, if not let the caller know that this function is no longer needed
     if np.array_equal(selected_features,prev_selected_features):
+        print("-----------------------------------------------------------------------------------------------")
+        print("We have found the unchanged set, thus from now on we are just adding to our selected features.\n")
+        print("-----------------------------------------------------------------------------------------------")
         return max_key,selected_features, selected_features_by_name, True
     else:
         return max_key,selected_features, selected_features_by_name, False
@@ -119,11 +124,90 @@ def forward_selection(data, X_train, y_train, X_val, y_val, max_rounds, thread_P
     highest_accu = 0
     best_feature = None
     selected_features_dict = []
-    # Boolean flag indicating whether we are to continue trying to swap out the 20 original features
-    isFinishedSwapping = False
-    
+
     # IIterations for amount of rounds we want to prefrorm (Note: max_rounds != number of features we will have at the end)
     for i in range(max_rounds):
+        # Keeping track of time, to monitor how long it takes for each iteration
+        start = time.time()
+        best_feature = None
+        highest_accu = 0
+        result = []
+        # Create a thread pool of thread_Pool amount of process
+        pool = multiprocessing.Pool(thread_Pool)
+        # Creating a dummy function in order to send multiple inputs to the function
+        func = partial(find_best_feature, selected_features, X_train, C_param, y_train)
+        # Gather the results
+        result = pool.map(func, features)
+        pool.close()
+        pool.join()
+        
+        #Iterate through the results, find the best_feature
+        for res in result:
+            if res[1] > highest_accu:
+                highest_accu = res[1]
+                best_feature = res[0]
+       
+         # We have converged and now just the best feature
+        selected_features = np.append(selected_features,best_feature)
+        selected_features_by_name.append(data.columns[best_feature])
+        
+        # Train the model again with these selected features
+        X_train_filt = X_train[:, selected_features]
+        X_val_filt = X_val[:, selected_features]
+        # Rank the data
+        X_train_filt_ranked = bin_rank(X_train_filt)
+        X_val_filt_ranked = bin_rank(X_val_filt)
+        # Train the model
+        lr = CategoricalNB(alpha=1.0, fit_prior=True, class_prior=None, min_categories=5).fit(X_train_filt_ranked, y_train)
+        #lr = LogisticRegression(C=C_param, random_state=0).fit(X_train_filt_ranked, y_train)
+        # Measure Validation accuracy with the features
+        accu_val = lr.score(X_val_filt_ranked, y_val)
+        # Remove the feature found from the list of features
+        features = np.setdiff1d(features, np.array([best_feature]))
+        # Measure time
+        times = time.time() - start
+        
+        # Populate dictionary of info and add it to our list and continue
+        selected_features_dict.append({"Feature": selected_features_by_name.copy(), "Iteration": i + 1,
+                                       "Training accuracy": highest_accu, "Validation Accuracy": accu_val,"Time": times})
+        print("Round:",i+1,"complete")
+    print("Finished: Total time can  be found below:")
+    # return a dictionary
+    return (selected_features_dict, highest_accu)
+
+
+"""
+Inputs: 
+- data: for getting feature names by index
+- X_train, y_train:  for training the model
+- X_val, y_val: for validating selected feature
+- thread_pool: how many threads we should use in our pool
+- selected_features(_by_name): the start to our feature selection list.
+Outputs:
+-selected_features_dict: contains all the information for every round of each subset of features at every adding interval and time it took to run
+Description:
+This function preforms forward feature selection, starting with an input of 20 features, we replace the 20 features until we have no change in the 20
+features. The program returns the 20 unchanged based features.
+"""
+def get_solid_20(data, X_train, y_train, X_val, y_val, thread_Pool,selected_features =[],
+                      selected_features_by_name = [], C_param=1):
+    # Getting all the features and remembering to remove the isLumA column
+    features = [i for i in range(data.shape[1] - 1)]
+    # Removing from the available features what we already have in the selected features
+    features = np.setdiff1d(features, selected_features)
+    # Maaking sure that are selected features contain what we already have
+    selected_features = selected_features
+    selected_features_by_name = selected_features_by_name
+    highest_accu = 0
+    best_feature = None
+    selected_features_dict = []
+    # Boolean flag indicating whether we are to continue trying to swap out the 20 original features
+    isFinishedSwapping = False
+    isExit = False
+    i = 0
+    
+    # IIterations for amount of rounds we want to prefrorm (Note: max_rounds != number of features we will have at the end)
+    while True:
         # Keeping track of time, to monitor how long it takes for each iteration
         start = time.time()
         best_feature = None
@@ -148,7 +232,10 @@ def forward_selection(data, X_train, y_train, X_val, y_val, max_rounds, thread_P
             # We still havent converged in our starting selected features, so we run the function to updated the selected features
             best_feature,selected_features, selected_features_by_name, isFinishedSwapping = selected_feature_check(
                 data,X_train, y_train,selected_features, selected_features_by_name, C_param,best_feature)
+            if isFinishedSwapping == True:
+                isExit = True   
         else:
+            isExit = True
             # We have converged and now just the best feature
             selected_features = np.append(selected_features,best_feature)
             selected_features_by_name.append(data.columns[best_feature])
@@ -160,7 +247,8 @@ def forward_selection(data, X_train, y_train, X_val, y_val, max_rounds, thread_P
         X_train_filt_ranked = bin_rank(X_train_filt)
         X_val_filt_ranked = bin_rank(X_val_filt)
         # Train the model
-        lr = LogisticRegression(C=C_param, random_state=0).fit(X_train_filt_ranked, y_train)
+        lr = CategoricalNB(alpha=1.0, fit_prior=True, class_prior=None,min_categories=5).fit(X_train_filt_ranked, y_train)
+        #lr = LogisticRegression(C=C_param, random_state=0).fit(X_train_filt_ranked, y_train)
         # Measure Validation accuracy with the features
         accu_val = lr.score(X_val_filt_ranked, y_val)
         # Remove the feature found from the list of features
@@ -171,6 +259,10 @@ def forward_selection(data, X_train, y_train, X_val, y_val, max_rounds, thread_P
         # Populate dictionary of info and add it to our list and continue
         selected_features_dict.append({"Feature": selected_features_by_name.copy(), "Iteration": i + 1,
                                        "Training accuracy": highest_accu, "Validation Accuracy": accu_val,"Time": times})
-        print("Round:",i,"Features found so far are: ", selected_features_by_name, "\n")
+        print("Round:",i+1,"complete")
+        i=i+1
+        if isExit:
+            break
+    print("Finished: Total time can  be found below:")
     # return a dictionary
     return (selected_features_dict, highest_accu);
